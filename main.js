9b42060e38c101b68c70000b7cdd0f5e0ab7a9df
isfetkos <sfetkosilias@gmail.com>
2022-12-30 12:45:23 +0100

    Second Try

--- main.js
+++ main.js
@@ -4,24 +4,38 @@ window.onload = function() {
   const toggleMenu = document.querySelector('.toggle-menu');
   const navList = document.querySelector('.nav-list');
   const container = document.querySelector('.container');
+  const logo = document.querySelector('.logo');
+  const toggleButton = document.querySelector('.toggle-button');
   
   // Add an event listener to the toggle button
   toggleMenu.addEventListener('click', function() {
     // Toggle the display property of the nav list
     if (navList.style.display === 'flex') {
       navList.style.display = 'none';
+      logo.style.display = 'block';
+      navList.style.flexDirection = 'row';
+
     } else {
       navList.style.display = 'flex';
-      toggleMenu.innerHTML = 'x'; // Set the innerHTML to the "x" symbol
-      container.style.flexDirection = "column";
+      logo.style.display = 'none';
+      navList.style.flexDirection = 'column';
     }
   });
+
+  toggleButton.addEventListener('click', () => {
+    toggleButton.classList.toggle('collapsed');
+  });
+
   // Add an event listener to the window
 window.addEventListener('resize', function() {
   // Check the width of the window
   if (window.innerWidth > 1000) {
     // If the width is greater than 780 pixels, show the nav list
     navList.style.display = 'flex';
+    container.style.flexDirection = "row"
+    navList.style.flexDirection = "row";
+    logo.style.display = 'block';
+
   } else {
     // If the width is less than or equal to 480 pixels, hide the nav list
     navList.style.display = 'none';
